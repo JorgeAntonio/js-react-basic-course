2801376e7e6b4feeb15232bfdf49d9d2f69d7eaa
JorgeAntonio <jorge.antonio.delaguila@gmail.com>
2025-08-16 10:55:22 -0500

    Add variable declarations and console logs for name, age, and adulthood status

---
+++ session1/prueba-1.js
@@ -0,0 +1,14 @@
+// Creemos variables: 
+// tu nombre (string), 
+// tu edad (número), 
+// y una variable esMayorDeEdad (booleano) basada en si tu edad es 18 o más. 
+// Luego, usa console.log para mostrar mensajes combinando estas variables.
+
+let nombre = 'Jorge'
+let edad = 30
+let esMayorDeEdad = edad >= 18
+
+console.log(`Nombre: ${nombre}`)
+console.log(`Edad: ${edad}`)
+console.log(`Es mayor de edad: ${esMayorDeEdad}`)
+
--- session1/session-1.js
+++ session1/session-1.js
@@ -1,1 +1,43 @@
-console.log('Hola Mundo')
+// MENSAJE DE CONSOLA
+// console.log('Hola Mundo')
+
+
+// VARIABLES Y CONSTANTES
+
+let edad = 30 // VARIABLE QUE CAMBIARAN CON EL TIEMPO
+const nombre = 'Jorge' // VARIABLE QUE NO CAMBIARA NUNCA
+var ciudad = 'Madrid' //NO USAR
+
+console.log('edad:', edad)
+console.log('nombre:', nombre)
+console.log('ciudad:', ciudad)
+
+// TIPOS DE DATOS
+
+let apellido = 'Del Aguila' // STRING
+let PI = 3.14 // NUMERICO
+let isMayor = false // true // BOOLEAN
+
+// CONCATENACIONES
+
+console.log(`Hola, mi nombre es ${nombre} y tengo ${edad} años`)
+
+// OPERACIONES BASICAS
+
+// SUMAR
+let a = 5
+let b = 10
+let suma = a + b 
+console.log('Suma:', suma)
+
+// RESTAR
+let resta = a - b 
+console.log('Resta:', resta)
+
+// MULTIPLICAR
+let multiplicar = a * b 
+console.log('Multiplicar:', multiplicar)
+
+// DIVIDIR
+let dividir = a / b 
+console.log('Dividir:', dividir)
